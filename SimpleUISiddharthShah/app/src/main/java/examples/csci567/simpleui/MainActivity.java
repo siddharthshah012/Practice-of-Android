97bdb76f0dc462be56b9a76e89fba51d8a23545d
siddharthshah012 <sshah8@mail.csuchico.edu>
2017-01-26 21:22:13 -0800

    First commit of creating Simple UI

---
+++ SimpleUISiddharthShah/app/src/main/java/examples/csci567/simpleui/MainActivity.java
@@ -0,0 +1,96 @@
+package examples.csci567.simpleui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+
+        View rootView;
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            Button btn1 = (Button)rootView.findViewById(R.id.button1);
+            btn1.setOnClickListener(this);
+            Button btn2 = (Button)rootView.findViewById(R.id.button2);
+            btn2.setOnClickListener(this);
+            Button btn3 = (Button)rootView.findViewById(R.id.button3);
+            btn3.setOnClickListener(this);
+
+            return rootView;
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch(v.getId())
+            {
+                case  R.id.button1:
+                    ((TextView)rootView.findViewById(R.id.textView)).setText("Button 1 Clicked");
+                    break;
+                case R.id.button2:
+                    ((TextView)rootView.findViewById(R.id.textView)).setText("Button 2 Clicked");
+                    break;
+                case R.id.button3:
+                    ((TextView)rootView.findViewById(R.id.title)).setText("Button 3 Clicked");
+                    break;
+
+
+            }
+        }
+    }
+}
